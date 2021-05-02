3b04b765b13f9deff65a0be5620dcb5bf660a337
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-05-02 22:42:12 +0200

    Replace thunk middleware promise middleware

--- src/data/actions/budget.actions.js
+++ src/data/actions/budget.actions.js
@@ -1,37 +1,13 @@
-import {
-  LOADING_STATES,
-  BUDGET_GET,
-  BUDGET_GET_REQUEST,
-  BUDGET_GET_SUCCESS,
-  BUDGET_GET_FAILURE,
-  BUDGETED_CATEGORIES_GET,
-  BUDGETED_CATEGORIES_REQUEST,
-  BUDGETED_CATEGORIES_SUCCESS,
-  BUDGETED_CATEGORIES_FAILURE,
-} from "data/constans";
+import { BUDGET_GET, BUDGETED_CATEGORIES_GET } from "data/constans";
 import API from "data/fetch";
 
-export const fetchBudget = (id) => async (dispatch) => {
-  dispatch({ type: BUDGET_GET_REQUEST });
-  try {
-    const response = await API.budget.fetchBudget(id);
-    const data = await response.json();
+export const fetchBudget = (id) => {
+  const promise = API.budget.fetchBudget(id);
 
-    dispatch({ type: BUDGET_GET_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: BUDGET_GET_FAILURE });
-  }
+  return { type: BUDGET_GET, promise };
 };
-export const fetchBudgetedCategories = (id) => async (dispatch) => {
-  dispatch({ type: BUDGETED_CATEGORIES_GET });
+export const fetchBudgetedCategories = (id) => {
+  const promise = API.budget.fetchBudgetedCategories(id);
 
-  try {
-    const response = await API.budget.fetchBudgetedCategories(id);
-    const data = await response.json();
-
-    dispatch({ type: BUDGETED_CATEGORIES_SUCCESS, payload: data });
-  } catch (error) {
-    console.log(error);
-    dispatch({ type: BUDGETED_CATEGORIES_FAILURE });
-  }
+  return { type: BUDGETED_CATEGORIES_GET, promise };
 };
---
+++ src/data/middlewares/promise.js
@@ -0,0 +1,29 @@
+import { BUDGET_GET_REQUEST } from "data/constans";
+
+export default function promiseMiddleware() {
+  return function (next) {
+    return function (action) {
+      const { promise, type, ...rest } = action;
+
+      if (!promise || typeof promise.then !== "function") {
+        return next(action);
+      }
+      const SUCCESS = `${type}_SUCCESS`;
+      const FAILURE = `${type}_FAILURE`;
+      const REQUEST = `${type}_REQUEST`;
+
+      next({ type: REQUEST, ...rest });
+
+      return promise
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          return next({ type: SUCCESS, payload: data, ...rest });
+        })
+        .catch((err) => {
+          console.log(err);
+          return next({ type: FAILURE, err, ...rest });
+        });
+    };
+  };
+}
